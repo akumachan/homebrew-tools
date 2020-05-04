a37230bd51fc2a7debc3f31b6ce6139f04967140
akumachan <akumachanit@gmail.com>
2020-05-04 13:03:39 +0900

    Corrected the Gip hash value.

--- gip.rb
+++ gip.rb
@@ -2,7 +2,7 @@ class Gip < Formula
   desc "Easily saving global IP address to clipboard."
   homepage "https://github.com/akumachan/gip"
   url "https://github.com/akumachan/gip/releases/download/0.0.3/gip"
-  sha256 "35caece914026609b6f7203a816ddc152318ba29"
+  sha256 "5eb67fdd3a6feda4a6aa96b1fd3c9678909d9aa8102a15bc4ab208f6f43fa80f"
 
   def install
 	bin.install "gip"
