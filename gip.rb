64d42d9e67a3dabcdfc01a5246d1673472c5d21b
akumachan <akumachanit@gmail.com>
2020-05-04 12:51:54 +0900

    Gip 0.0.3 release.

--- gip.rb
+++ gip.rb
@@ -1,14 +1,14 @@
 class Gip < Formula
   desc "Easily saving global IP address to clipboard."
   homepage "https://github.com/akumachan/gip"
-  url "https://github.com/akumachan/gip/releases/download/0.0.2/gip"
-  sha256 "181ea4229ecf522dae70b0db3c26bc732bdc95d12eac649dbd9904f8cd2c045e"
+  url "https://github.com/akumachan/gip/releases/download/0.0.3/gip"
+  sha256 "35caece914026609b6f7203a816ddc152318ba29"
 
   def install
 	bin.install "gip"
   end
 
   test do
-    system "false"
+    system "true"
   end
 end
